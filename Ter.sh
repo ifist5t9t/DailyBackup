c436cf749744e1d1a3d22fd135fc32e49801e02e
ifist5t9t <53459655+ifist5t9t@users.noreply.github.com>
2024-06-07 14:22:29 +0100

    Update Ter.sh

--- Ter.sh
+++ Ter.sh
@@ -1,9 +1,9 @@
 #/data/data/com.termux/files/usr/bin/bash -e 
 apt update ; apt upgrade --assume=yes 
 apt install gradle maven groovy vim gdb wget python git golang python man clamav python                   python-lxml              python-tkinterpython-apsw              python-msgpack           python-tldppython-apt               python-numpy             python-torchpython-bcrypt            python-numpy-static      python-torch-staticpython-contourpy         python-pillow            python-torchaudiopython-cryptography      python-pip               python-torchvisionpython-ensurepip-wheels  python-pyarrow           python-xcbgenpy -y
-fresh
+# fresh
 echo "import random , os ,\n # randomizer task and integrity "
-cat /storage/../Android/data/com.termux/.bash.bashrc > ../usr/etc/bash.bashrc
+#cat /storage/../Android/data/com.termux/.bash.bashrc > ../usr/etc/bash.bashrc
 # Metasploit 6
 git clone https://github.com/gushmazuko/metasploit_in_termux
 # Android-sdk 
