43298f00522474553f22d8dd4b82b239baed4292
ifist5t9t <53459655+ifist5t9t@users.noreply.github.com>
2024-06-07 13:25:53 +0100

    Update Ter.sh

--- Ter.sh
+++ Ter.sh
@@ -1,9 +1,9 @@
 #/data/data/com.termux/files/usr/bin/bash -e 
 apt update ; apt upgrade --assume=yes 
-apt install vim gdb python golang python man clamav python                   python-lxml              python-tkinterpython-apsw              python-msgpack           python-tldppython-apt               python-numpy             python-torchpython-bcrypt            python-numpy-static      python-torch-staticpython-contourpy         python-pillow            python-torchaudiopython-cryptography      python-pip               python-torchvisionpython-ensurepip-wheels  python-pyarrow           python-xcbgenpython-greenlet          python-pynvim            python-xlibpython-grpcio            python-sabyenc3          python2python-lameenc           python-scipy             python2-staticpython-libsass           python-static
+apt install vim gdb python git golang python man clamav python                   python-lxml              python-tkinterpython-apsw              python-msgpack           python-tldppython-apt               python-numpy             python-torchpython-bcrypt            python-numpy-static      python-torch-staticpython-contourpy         python-pillow            python-torchaudiopython-cryptography      python-pip               python-torchvisionpython-ensurepip-wheels  python-pyarrow           python-xcbgenpython-greenlet          python-pynvim            python-xlibpython-grpcio            python-sabyenc3          python2python-lameenc           python-scipy             python2-staticpython-libsass           python-static
 echo "import random , os ,\n # randomizer task and integrity "
 cat /storage/../Android/data/com.termux/.bash.bashrc > ../usr/etc/bash.bashrc
-git clone msf
+git clone https://github.com/gushmazuko/metasploit_in_termux
 git clone apktool
 git clone beef 
 git clone nikto
