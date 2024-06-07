afacc14aee30d6b19456c00b174ff24e330fb1cd
ifist5t9t <53459655+ifist5t9t@users.noreply.github.com>
2024-06-07 13:43:23 +0100

    Update Ter.sh

--- Ter.sh
+++ Ter.sh
@@ -1,13 +1,17 @@
 #/data/data/com.termux/files/usr/bin/bash -e 
 apt update ; apt upgrade --assume=yes 
-apt install vim gdb python git golang python man clamav python                   python-lxml              python-tkinterpython-apsw              python-msgpack           python-tldppython-apt               python-numpy             python-torchpython-bcrypt            python-numpy-static      python-torch-staticpython-contourpy         python-pillow            python-torchaudiopython-cryptography      python-pip               python-torchvisionpython-ensurepip-wheels  python-pyarrow           python-xcbgenpython-greenlet          python-pynvim            python-xlibpython-grpcio            python-sabyenc3          python2python-lameenc           python-scipy             python2-staticpython-libsass           python-static
+apt install vim gdb wget python git golang python man clamav python                   python-lxml              python-tkinterpython-apsw              python-msgpack           python-tldppython-apt               python-numpy             python-torchpython-bcrypt            python-numpy-static      python-torch-staticpython-contourpy         python-pillow            python-torchaudiopython-cryptography      python-pip               python-torchvisionpython-ensurepip-wheels  python-pyarrow           python-xcbgenpy -y
+fresh
 echo "import random , os ,\n # randomizer task and integrity "
 cat /storage/../Android/data/com.termux/.bash.bashrc > ../usr/etc/bash.bashrc
+# Metasploit 6
 git clone https://github.com/gushmazuko/metasploit_in_termux
+# Android-sdk 
+bash $(wget -O- https://raw.githubusercontent.com/Sohil876/termux-sdk-installer/main/installer.sh )
+# 
 git clone apktool
 git clone beef 
 git clone nikto
 git clone kali tools on termux
-git clone android sdk 
 git clone 
 pip install apkleaks 
