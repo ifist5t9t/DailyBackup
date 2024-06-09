55e644801e4634c59e8d5f34b930e46ab09b0e78
ifist5t9t <53459655+ifist5t9t@users.noreply.github.com>
2024-06-09 12:13:39 +0100

    Update Ter.sh

--- Ter.sh
+++ Ter.sh
@@ -2,7 +2,7 @@
 
 
 apt update ; apt upgrade --assume=yes 
-apt install nginx gradle maven nodejs groovy vim gdb wget python git golang python man clamav python                   python-lxml                           python-msgpack           python-tldppython-apt               python-numpy             python-torchpython-bcrypt            python-numpy-static      python-torch-staticpython-contourpy         python-pillow            python-torchaudiopython-cryptography      python-pip               python-torchvisionpython-ensurepip-wheels  python-pyarrow           python-xcbgenpy -y
+apt install nginx gradle maven nodejs groovy vim gdb wget python git golang python man clamav python     python                   python-pip python-apsw              python-pyarrow python-apt               python-pynvim python-bcrypt            python-sabyenc3 python-contourpy         python-scipy python-cryptography      python-static python-ensurepip-wheels  python-tkinter python-greenlet          python-tldp python-grpcio            python-torch python-lameenc           python-torch-static python-libsass           python-torchaudio python-lxml              python-torchvision python-msgpack           python-xcbgen python-numpy             python-xlib python-numpy-static      python2 python-pillow            python2-static  -y
 # fresh
 echo "import random , os ,\n # randomizer task and integrity "
 #cat /storage/../Android/data/com.termux/.bash.bashrc > ../usr/etc/bash.bashrc
