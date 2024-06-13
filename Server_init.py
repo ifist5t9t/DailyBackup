2abd50ac32a730e1f541010e0433d27925723e02
ifist5t9t <53459655+ifist5t9t@users.noreply.github.com>
2024-06-13 14:18:41 +0100

    Create Server_init.py
    Hidden Source

---
+++ Server_init.py
@@ -0,0 +1,12 @@
+#!/bin/python 
+
+import time,hashlib,pathlib,socket,sqlite,thread,random
+
+host = '10.0.0.7'
+port = random.random()
+
+while True:
+  blblblblbl
+  blblblblbl
+  ----> Inside h have a virtual digt otherwise unlimit <----
+  #🖕 unhackable. try me if you can ....
